9709ba20a108676e190dbfe7db643749efb58a72
Kevin Moore <k.james.moore@gmail.com>
2016-07-07 08:29:15 -0400

    Thought out frontend and backend routes.
    Could also use Zapier, etc, but structuring for home-rolled at this point.

--- app.py
+++ app.py
@@ -2,9 +2,37 @@ from flask import Flask
 app = Flask(__name__)
 
 
+#
+# FRONTEND ROUTES
+#
+
+#  Subscribe to poll results page
 @app.route('/')
 def hello():
     return "Hello World!"
+
+
+# Display poll results page, but only if proper secret.
+
+
+
+
+#
+# API ROUTES
+#
+
+# User subscribes to a poll,
+# ---> send user an email
+# ---> prepare row in database
+
+
+# Recording poll results
+# -->
+
+
+# Retrieve results of a poll
+
+
 
 if __name__ == '__main__':
     app.run()
